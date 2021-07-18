3e58bd4ae3336d828cb05c0b1caff06bde4ddb22
Loic Jacquel <ljacquel@gmail.com>
2021-07-18 11:55:41 +0900

    test

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-require('dotenv').config();
-const MartingaleSyncStrategy = require('./strategy/martingaleasync');
-const MartingaleStrategy = require('./strategy/martingale');
 
-const CONFIG = {
-    symbol: 'BTCUSDT',
-    leverage:10, 
-    balance:1000,
-    targetPercent:0.004,
-    positionPercent:0.01,
-    mode:'binance'
-}
-
-const executeStrategy = () => {
-    return new Promise(async (resolve, reject) => {
-        let strategy = new MartingaleSyncStrategy(CONFIG);
-        if(strategy.init) {
-            await strategy.init();
-        }
-        strategy.start();
-        resolve();
-    });
-}
-
-  
 const start = async () => {
-
-    require('./startup/database').init();
-    executeStrategy();
 
     const express = require('express')
     const app = express()
---
+++ index2.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const MartingaleSyncStrategy = require('./strategy/martingaleasync');
+const MartingaleStrategy = require('./strategy/martingale');
+
+const CONFIG = {
+    symbol: 'BTCUSDT',
+    leverage:10, 
+    balance:1000,
+    targetPercent:0.004,
+    positionPercent:0.01,
+    mode:'binance'
+}
+
+const executeStrategy = () => {
+    return new Promise(async (resolve, reject) => {
+        let strategy = new MartingaleSyncStrategy(CONFIG);
+        if(strategy.init) {
+            await strategy.init();
+        }
+        strategy.start();
+        resolve();
+    });
+}
+
+  
+const start = async () => {
+
+    require('./startup/database').init();
+    executeStrategy();
+
+    const express = require('express')
+    const app = express()
+    const port = process.env.PORT || 3000;
+      
+    app.get ("/", function (req,res) {
+        res.render ( "index.ejs" );	
+    });
+    
+    app.listen(port, () => {
+        console.log(`Listening at http://localhost:${port}`)
+    })
+}
+
+start();
+//------------
+//------------
