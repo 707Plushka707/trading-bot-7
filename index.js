8daed2b34b6afe0a9f7dfa45af2300fbfe2fed8f
Loic Jacquel <ljacquel@gmail.com>
2021-07-18 11:02:40 +0900

    add home page

--- index.js
+++ index.js
@@ -28,17 +28,17 @@ const start = async () => {
     require('./startup/database').init();
     executeStrategy();
 
-    // const express = require('express')
-    // const app = express()
-    // const port = process.env.PORT || 3000;
+    const express = require('express')
+    const app = express()
+    const port = process.env.PORT || 3000;
       
-    // app.get ("/", function (req,res) {
-    //     res.render ( "index.ejs" );	
-    // });
+    app.get ("/", function (req,res) {
+        res.render ( "index.ejs" );	
+    });
     
-    // app.listen(port, () => {
-    //     console.log(`Listening at http://localhost:${port}`)
-    // })
+    app.listen(port, () => {
+        console.log(`Listening at http://localhost:${port}`)
+    })
 }
 
 start();
